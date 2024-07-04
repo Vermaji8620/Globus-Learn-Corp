8522d817af7e1ea150c57c1c74350e6cc0f291de
vermaji <rajaditya14032002@gmail.com>
2024-07-04 21:45:55 +0530

    server setup done, gitignore file made, environment file made, basic dependencies installed

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+// import userRoute from "./routes/user.route.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send(`This is a testing URL`);
+});
+
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("hoga");
+//     next();
+//   },
+//   userRoute
+// );
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: `Error caught in global catch`,
+    error: err.message,
+  });
+});
+
+(async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://vadityaraj67:${process.env.MONGODB_CLUSTER}@cluster0.yvftnur.mongodb.net/Globus-Learn-Corp`
+    );
+
+    console.log("Database connected");
+
+    app.listen(PORT, () => {
+      console.log(`Your server is up and running at port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to the database:", error.message);
+  }
+})();
