cb4d023375b21f5cafd4fa9a319b0e2b48a5107b
vermaji <rajaditya14032002@gmail.com>
2024-07-04 21:58:19 +0530

    made model for the user authentication

---
+++ models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+// writing down the basic schema here
+const user = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    universityRoll: {
+      type: Number,
+      unique: true,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", user);
