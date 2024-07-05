1797f065c88e86b689b500dda12acb01fa5859b2
vermaji <rajaditya14032002@gmail.com>
2024-07-05 18:05:00 +0530

    middlewares added for all the routes

--- routes/course.route.js
+++ routes/course.route.js
@@ -7,11 +7,12 @@ import {
   updateCourse,
   validateCourse,
 } from "../controllers/course.controller.js";
+import { userVerification } from "../controllers/user.controller.js";
 const router = express.Router();
 
-router.post("/createcourse", validateCourse, createCourse);
+router.post("/createcourse", userVerification, validateCourse, createCourse);
 router.get("/getcourse", getCourse);
-router.get("/getcourse/:id", getspecificCourse);
-router.put("/updatecourse/:id", updateCourse);
-router.delete("/deletecourse/:id", deleteCourse);
+router.get("/getcourse/:id", userVerification, getspecificCourse);
+router.put("/updatecourse/:id", userVerification, updateCourse);
+router.delete("/deletecourse/:id", userVerification, deleteCourse);
 export default router;
--- routes/question.route.js
+++ routes/question.route.js
@@ -1,8 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { createQuestion, getQuestions } from "../controllers/question.controller.js";
+import {
+  createQuestion,
+  getQuestions,
+} from "../controllers/question.controller.js";
+import { userVerification } from "../controllers/user.controller.js";
 
-router.post("/createquestion", createQuestion);
-router.get("/getquestions/:courseId", getQuestions);
+router.post("/createquestion", userVerification, createQuestion);
+router.get("/getquestions/:courseId", userVerification, getQuestions);
 
 export default router;
