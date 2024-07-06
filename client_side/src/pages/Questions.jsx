0f4fe3bb0fe270a87420f6880d87362ea38ae0d1
vermaji <rajaditya14032002@gmail.com>
2024-07-06 14:30:47 +0530

    every question and course details for every course is getting fetched

--- client_side/src/pages/Questions.jsx
+++ client_side/src/pages/Questions.jsx
@@ -5,7 +5,8 @@ import { useEffect, useState } from "react";
 
 const Questions = () => {
   const [questionsarray, setQuestionsarray] = useState([]);
-  const navigate = useNavigate();
+  const [specificCourse, setSpecificCourse] = useState({});
+  const navigate = useNavigate(); 
   useEffect(() => {
     if (!localStorage.getItem("id")) navigate("/login");
   }, [navigate]);
@@ -15,6 +16,9 @@ const Questions = () => {
       const data = await fetch("/question/getquestions/" + courseId);
       const res = await data.json();
       setQuestionsarray(res);
+      const coursedata = await fetch("/course/getcourse/" + courseId);
+      const courseres = await coursedata.json();
+      setSpecificCourse(courseres.course)
     })();
   }, [navigate, courseId]);
   return (
@@ -23,12 +27,12 @@ const Questions = () => {
         <div className="flex flex-col gap-6">
           <img src={logofrocourse} className="rounded-[50px]" alt="" />
           <div className="pt-10 pl-6 bg-white rounded-[20px] pb-6 gap-6 flex-col flex">
-            <p className="font-bold">Name of the course  -  </p>
-            <p className="font-bold">Description of the course  -  </p>
-            <p className="font-bold">By   -  </p>
-            <p className="font-bold">$5000  -  </p>
-            <p className="font-bold">Rating  -  </p>
-            <p className="font-bold">Duration  -  </p>
+            <p><span className="font-bold">Name of the course</span> - {specificCourse.name}</p>
+            <p><span className="font-bold">Description of the course</span> - {specificCourse.description}</p>
+            <p><span className="font-bold">By</span> - {specificCourse.teacher}</p>
+            <p><span className="font-bold">Price</span> - ${specificCourse.price}</p>
+            <p><span className="font-bold">Rating</span> - {specificCourse.rating}</p>
+            <p><span className="font-bold">Duration</span> - {specificCourse.durationInHours}</p>
           </div>
         </div>
       </div>
